3312663968c034fd239504c35f9df6dbcd328ec1
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-25 18:13:42 -0500

    Get rule 30 rendering correctly

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const C_WIDTH = 600;
 const C_HEIGHT = 400;
-const GRID_SIZE = 2;
+const GRID_SIZE = 10;
 
 const canvas = document.getElementById("world");
 canvas.width = C_WIDTH;
 canvas.height = C_HEIGHT;
 const ctx = canvas.getContext('2d');
 
+let iterNum = 1;
+
 // initialize the bottom row with random numbers
-const bottomRow = [];
+let bottomRow = [];
 for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
   bottomRow.push(Math.round(Math.random()));
 }
+// initialize the bottom row with a single pixel in the middle
+for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
+  bottomRow[i] = 0;
+  if (i === ~~(C_WIDTH / GRID_SIZE / 2)) {
+    bottomRow[i] = 1;
+  }
+}
+
+// draw the bottom row
 for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
   if (bottomRow[i]) {
-    ctx.rect(
+    ctx.fillRect(
       i * GRID_SIZE,
       C_HEIGHT - GRID_SIZE,
       GRID_SIZE,
@@ -23,13 +34,8 @@ for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
 }
 ctx.fill();
 
-// copy the previous row higher after the next iteration
-const data = ctx.getImageData(
-  0, C_HEIGHT - GRID_SIZE, C_WIDTH, GRID_SIZE);
-ctx.putImageData(data, 0, C_HEIGHT - 2 * GRID_SIZE);
-
 // rule 30
-const nextIter = {
+const NEXT_ITER = {
   '111': 0,
   '110': 0,
   '101': 0,
@@ -39,21 +45,36 @@ const nextIter = {
   '001': 1,
   '000': 0,
 };
-const nextRow = [];
-for (i = 0, n = C_WIDTH / GRID_SIZE; i < n; i++) {
-  const nextKey = 
-    (bottomRow[i - 1] || 0).toString() +
-    bottomRow[i].toString() +
-    (bottomRow[i + 1] || 0).toString();
-  nextRow.push(nextIter[nextKey]);
-}
-for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
-  if (nextRow[i]) {
-    ctx.rect(
-      i * GRID_SIZE,
-      C_HEIGHT - GRID_SIZE,
-      GRID_SIZE,
-      GRID_SIZE);
+
+setInterval(() => {
+  // move all the previously-calculated rows up a row
+  const data = ctx.getImageData(
+    0, GRID_SIZE, C_WIDTH, C_HEIGHT - GRID_SIZE);
+  ctx.putImageData(data, 0, 0);
+
+  // calculate the values of the row in the next iteration
+  const nextRow = [];
+  for (i = 0, n = C_WIDTH / GRID_SIZE; i < n; i++) {
+    const nextKey =
+      (bottomRow[i - 1] || 0).toString() +
+      bottomRow[i].toString() +
+      (bottomRow[i + 1] || 0).toString();
+    nextRow.push(NEXT_ITER[nextKey]);
   }
-}
-ctx.fill();
+  // console.log(nextRow);
+
+  // draw the new bottom row with the values of the new iteration
+  ctx.clearRect(0, C_HEIGHT - GRID_SIZE, C_WIDTH, GRID_SIZE);
+  for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
+    if (nextRow[i] === 1) {
+      ctx.fillRect(
+        i * GRID_SIZE,
+        C_HEIGHT - GRID_SIZE,
+        GRID_SIZE,
+        GRID_SIZE);
+    }
+  }
+
+  bottomRow = nextRow.slice();
+  iterNum += 1;
+}, 500);
