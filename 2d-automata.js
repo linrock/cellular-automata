f6b4d8dfcd3aa1a29e1518fc9def1ce02bc991d7
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-26 13:03:46 -0500

    Placeholder code for generating new worlds

--- 2d-automata.js
+++ 2d-automata.js
@@ -11,26 +11,43 @@
 
   const ctx = canvas.getContext('2d');
 
-  // initialize the board with random numbers
-  const board = [];
+  let world = [];
+
+  // initialize the world with random numbers
   for (let y = 0; y < C_HEIGHT / GRID_SIZE; y++) {
     const row = [];
     for (let x = 0; x < C_WIDTH / GRID_SIZE; x++) {
       row.push(Math.random() > 0.5 ? 1 : 0);
     }
-    board.push(row);
+    world.push(row);
   }
 
-  // draw the board
-  for (let y = 0; y < C_HEIGHT / GRID_SIZE; y++) {
-    for (let x = 0; x < C_WIDTH / GRID_SIZE; x++) {
-      if (board[y][x] === 1) {
-        ctx.fillRect(
-          x * GRID_SIZE,
-          y * GRID_SIZE,
-          GRID_SIZE,
-          GRID_SIZE);
+  function calculateNewWorld(world) {
+    return world;
+  }
+
+  function drawWorld(world) {
+    ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+    for (let y = 0; y < C_HEIGHT / GRID_SIZE; y++) {
+      for (let x = 0; x < C_WIDTH / GRID_SIZE; x++) {
+        if (world[y][x] === 1) {
+          ctx.fillRect(
+            x * GRID_SIZE,
+            y * GRID_SIZE,
+            GRID_SIZE,
+            GRID_SIZE);
+        }
       }
     }
   }
+
+  function updateForever() {
+    const newWorld = calculateNewWorld(world);
+    requestAnimationFrame(() => drawWorld(newWorld));
+    world = newWorld;
+    setTimeout(() => updateForever(), UPDATE_INTERVAL_MS);
+  }
+
+  drawWorld(world);
+  updateForever();
 })();
