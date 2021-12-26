256ef4c1216021ab31ed17f84fa72d6b1ba781ad
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-26 15:35:42 -0500

    Use numbers instead of strings in 1d automata

--- 1d-automata.js
+++ 1d-automata.js
@@ -21,27 +21,27 @@
     // initialize the bottom row with random numbers
     RANDOM_UNIFORM: () => {
       for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
-        bottomRow.push(Math.random() > 0.5 ? '1' : '0');
+        bottomRow.push(Math.random() > 0.5 ? 1 : 0);
       }
     },
     // initialize the bottom row with a single pixel in the middle
     ONE_MIDDLE: () => {
       for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
-        bottomRow[i] = '0';
+        bottomRow[i] = 0;
         if (i === ~~(C_WIDTH / GRID_SIZE / 2)) {
-          bottomRow[i] = '1';
+          bottomRow[i] = 1;
         }
       }
     },
   };
 
   let bottomRow = [];
-  INIT.RANDOM_UNIFORM();
-  // INIT.ONE_MIDDLE();
+  // INIT.RANDOM_UNIFORM();
+  INIT.ONE_MIDDLE();
 
   // draw the bottom row
   for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
-    if (bottomRow[i] === '1') {
+    if (bottomRow[i]) {
       ctx.fillRect(
         i * GRID_SIZE,
         C_HEIGHT - GRID_SIZE,
@@ -51,19 +51,10 @@
   }
 
   // converts a rule number to a 8-digit binary string
-  const RULE_BIN = RULE_NUM.toString(2).padStart(8, '0');
-  const ITER_KEYS = [
-    '111',
-    '110',
-    '101',
-    '100',
-    '011',
-    '010',
-    '001',
-    '000',
-  ];
+  const RULE_BIN = RULE_NUM.toString(2).padStart(8, 0);
+  const ITER_KEYS = [7, 6, 5, 4, 3, 2, 1, 0];
   const ITER_MAP = {};
-  ITER_KEYS.forEach((k, i) => ITER_MAP[k] = RULE_BIN[i]);
+  ITER_KEYS.forEach((k, i) => ITER_MAP[k] = parseInt(RULE_BIN[i]));
 
   // calculate the values of the next row based on the previous row
   function calculateNextRow(prevRow) {
@@ -72,11 +63,11 @@
     for (i = 0; i < n; i++) {
       let nextKey;
       if (i === 0) {
-        nextKey = prevRow[n - 1] + prevRow[i] + prevRow[i + 1];
+        nextKey = 4 * prevRow[n - 1] + 2 * prevRow[i] + prevRow[i + 1];
       } else if (i === n - 1) {
-        nextKey = prevRow[i - 1] + prevRow[i] + prevRow[0];
+        nextKey = 4 * prevRow[i - 1] + 2 * prevRow[i] + prevRow[0];
       } else {
-        nextKey = prevRow[i - 1] + prevRow[i] + prevRow[i + 1];
+        nextKey = 4 * prevRow[i - 1] + 2 * prevRow[i] + prevRow[i + 1];
       }
       nextRow.push(ITER_MAP[nextKey]);
     }
@@ -92,7 +83,7 @@
     // draw the new bottom row with the values of the new iteration
     ctx.clearRect(0, C_HEIGHT - GRID_SIZE, C_WIDTH, GRID_SIZE);
     for (let i = 0; i < C_WIDTH / GRID_SIZE; i++) {
-      if (nextRow[i] === '1') {
+      if (nextRow[i]) {
         ctx.fillRect(
           i * GRID_SIZE,
           C_HEIGHT - GRID_SIZE,
