34c307c3f48ecb665b55793a4f0c85ca815f1e04
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-28 11:07:20 -0500

    Redraw the whole world with rule 90 animation too

--- js/rule-90.js
+++ js/rule-90.js
@@ -9,8 +9,6 @@
   const NUM_CELLS_X = C_WIDTH / GRID_SIZE;
   const NUM_CELLS_Y = C_HEIGHT / GRID_SIZE;
 
-  const RULE_NUM = 90;
-
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = '#39FF14';
 
@@ -18,41 +16,45 @@
   canvas.addEventListener('play', () => isAnimating = true);
   canvas.addEventListener('pause', () => isAnimating = false);
 
-  function drawCells(cells) {
-    requestAnimationFrame(() => {
-      // shift all rows up by one. removes the top row
-      const data = ctx.getImageData(
-        0, GRID_SIZE, C_WIDTH, C_HEIGHT - GRID_SIZE);
-      ctx.putImageData(data, 0, 0);
+  // initialize an empty world
+  let world = [];
+  for (let i = 0; i < NUM_CELLS_Y; i++) {
+    world.push(new Array(NUM_CELLS_X).fill(0));
+  }
 
-      // draw the new bottom row with the values of the new generation
-      ctx.clearRect(0, C_HEIGHT - GRID_SIZE, C_WIDTH, GRID_SIZE);
-      for (let i = 0; i < NUM_CELLS_X; i++) {
-        if (cells[i]) {
-          ctx.fillRect(
-            i * GRID_SIZE,
-            C_HEIGHT - GRID_SIZE,
-            GRID_SIZE,
-            GRID_SIZE);
+  // initialize an elementary cellular automaton
+  const RULE_NUM = 90;
+  const eca = new ECA(NUM_CELLS_X, RULE_NUM, 'one_middle');
+
+  function drawWorld(world) {
+    requestAnimationFrame(() => {
+      ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+      for (let x = 0; x < NUM_CELLS_X; x++) {
+        for (let y = 0; y < NUM_CELLS_Y; y++) {
+          if (world[y][x]) {
+            ctx.fillRect(
+              x * GRID_SIZE,
+              y * GRID_SIZE,
+              GRID_SIZE,
+              GRID_SIZE);
+          }
         }
       }
     });
   }
 
-  function updateForever(eca) {
+  function updateForever() {
     if (isAnimating) {
       eca.calculateNextGeneration();
-      drawCells(eca.cells);
+      world.push(eca.cells);
+      world.shift();
+      drawWorld(world);
     }
     setTimeout(() => updateForever(eca), UPDATE_INTERVAL_MS);
   }
 
-  // initialize an elementary cellular automaton
-  const eca = new ECA(NUM_CELLS_X, RULE_NUM, 'one_middle'); 
-
-  // draw the bottom row
-  drawCells(eca.cells);
-
-  // forever repeat calculating new rows and drawing them
-  updateForever(eca);
+  world.push(eca.cells);
+  world.shift();
+  drawWorld(world);      // draw the whole world
+  updateForever();       // forever repeat calculating/drawing new rows
 })();
