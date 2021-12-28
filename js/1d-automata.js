bbbe82fe90c042f525c2b66492f9403225505dac
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-28 16:10:48 -0500

    Add a second 1D ECA canvas, also chaotic

--- js/1d-automata.js
+++ js/1d-automata.js
@@ -6,29 +6,58 @@
   // const RULE_NUM = 110;
   // const RULE_NUM = 184;
 
-  const anim = new AnimatedCanvas('1d-world', 3, (numCellsX, numCellsY) => {
-    // initialize an elementary cellular automaton
-    const eca = new ECA(numCellsX, RULE_NUM, 'one_middle');
+  {
+    const anim = new AnimatedCanvas('1d-world', 3, (numCellsX, numCellsY) => {
+      // initialize an elementary cellular automaton
+      const eca = new ECA(numCellsX, RULE_NUM, 'one_middle');
 
-    // initialize an empty world
-    const world = [];
-    for (let i = 0; i < numCellsY; i++) {
-      world.push(new Array(numCellsX).fill(0));
-    }
+      // initialize an empty world
+      const world = [];
+      for (let i = 0; i < numCellsY; i++) {
+        world.push(new Array(numCellsX).fill(0));
+      }
 
-    // the first ECA generation is the bottom row of the world
-    world.push(eca.cells);
-    world.shift();
-
-    // add new generations of the ECA to the bottom
-    return () => {
-      eca.calculateNextGeneration();
+      // the first ECA generation is the bottom row of the world
       world.push(eca.cells);
       world.shift();
-      return world;
-    };
-  });
-  anim.setForegroundColor('#cef9f2');  // light blue
-  anim.setBackgroundColor('#1b065e');
-  anim.updateForever();
+
+      // add new generations of the ECA to the bottom
+      return () => {
+        eca.calculateNextGeneration();
+        world.push(eca.cells);
+        world.shift();
+        return world;
+      };
+    });
+    anim.setForegroundColor('#cef9f2');  // light blue
+    anim.setBackgroundColor('#1b065e');
+    anim.updateForever();
+  }
+
+  {
+    const anim = new AnimatedCanvas('1d-world-2', 3, (numCellsX, numCellsY) => {
+      const eca = new ECA(numCellsX, 106, 'random');
+
+      // initialize an empty world
+      const world = [];
+      for (let i = 0; i < numCellsY; i++) {
+        world.push(new Array(numCellsX).fill(0));
+      }
+
+      // the first ECA generation is the bottom row of the world
+      world.push(eca.cells);
+      world.shift();
+
+      // add new generations of the ECA to the bottom
+      return () => {
+        eca.calculateNextGeneration();
+        world.push(eca.cells);
+        world.shift();
+        return world;
+      };
+    });
+    anim.setForegroundColor('#cef9f2');  // light blue
+    anim.setBackgroundColor('#1b065e');
+    anim.updateForever();
+  }
 })();
