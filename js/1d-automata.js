adeb3ee1f73ef514abfcac24ac3b557498df3a79
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-28 13:14:34 -0500

    Use animated canvas class for ECA section animation

--- js/1d-automata.js
+++ js/1d-automata.js
@@ -1,13 +1,4 @@
 (() => {
-  const C_WIDTH = 330;
-  const C_HEIGHT = 165;
-  const GRID_SIZE = 3;
-
-  const UPDATE_INTERVAL_MS = 50;
-
-  const NUM_CELLS_X = C_WIDTH / GRID_SIZE;
-  const NUM_CELLS_Y = C_HEIGHT / GRID_SIZE;
-
   // https://mathworld.wolfram.com/ElementaryCellularAutomaton.html
   const RULE_NUM = 30;   // chaotic
   // const RULE_NUM = 70;
@@ -15,75 +6,29 @@
   // const RULE_NUM = 110;
   // const RULE_NUM = 184;
 
-  // const FG_COLOR = '#39FF14'; // light green
-  const FG_COLOR = '#cef9f2'; // light blue
-  const BG_COLOR = '#1b065e';
+  const anim = new AnimatedCanvas('1d-world', 3, (numCellsX, numCellsY) => {
+    // initialize an elementary cellular automaton
+    const eca = new ECA(numCellsX, RULE_NUM, 'one_middle');
 
-  const canvas = document.getElementById('1d-world');
-  canvas.width = C_WIDTH;
-  canvas.height = C_HEIGHT;
-  canvas.style.background = BG_COLOR;
-
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = FG_COLOR;
-
-  let world = [];
-  for (let i = 0; i < NUM_CELLS_Y; i++) {
-    world.push(new Array(NUM_CELLS_X).fill(0));
-  }
-
-  // initialize an elementary cellular automaton
-  let eca = new ECA(NUM_CELLS_X, RULE_NUM, 'one_middle');
-
-  let isAnimating = false;
-  canvas.addEventListener('play', () => isAnimating = true);
-  canvas.addEventListener('pause', () => isAnimating = false);
-  canvas.addEventListener('click', () => {
-    // click to clear the canvas and start over
-    isAnimating = false;
-    requestAnimationFrame(() => {
-      ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
-    });
-    world = [];
-    for (let i = 0; i < NUM_CELLS_Y; i++) {
-      world.push(new Array(NUM_CELLS_X).fill(0));
+    // initialize an empty world
+    const world = [];
+    for (let i = 0; i < numCellsY; i++) {
+      world.push(new Array(numCellsX).fill(0));
     }
-    eca = new ECA(NUM_CELLS_X, RULE_NUM, 'one_middle');
+
+    // the first ECA generation is the bottom row of the world
     world.push(eca.cells);
     world.shift();
-    drawWorld(world);
-    isAnimating = true;
-  });
 
-  function drawWorld(world) {
-    requestAnimationFrame(() => {
-      ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
-      for (let x = 0; x < NUM_CELLS_X; x++) {
-        for (let y = 0; y < NUM_CELLS_Y; y++) {
-          if (world[y][x]) {
-            ctx.fillRect(
-              x * GRID_SIZE,
-              y * GRID_SIZE,
-              GRID_SIZE,
-              GRID_SIZE);
-          }
-        }
-      }
-    });
-  }
-
-  function updateForever() {
-    if (isAnimating) {
+    // add new generations of the ECA to the bottom
+    return () => {
       eca.calculateNextGeneration();
       world.push(eca.cells);
       world.shift();
-      drawWorld(world);
-    }
-    setTimeout(() => updateForever(), UPDATE_INTERVAL_MS);
-  }
-
-  world.push(eca.cells);
-  world.shift();
-  drawWorld(world);      // draw the whole world
-  updateForever();       // forever repeat calculating/drawing new rows
+      return world;
+    };
+  });
+  anim.setForegroundColor('#cef9f2');  // light blue
+  anim.setBackgroundColor('#1b065e');
+  anim.updateForever();
 })();
