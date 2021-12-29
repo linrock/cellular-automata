76a8e662479e7ea45042eaff1682110d047d70a6
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-28 22:03:31 -0500

    Get a janky cyclic 2d automata working

---
+++ js/2d-cyclic.js
@@ -0,0 +1,34 @@
+(() => {
+  // cyclic automata with random initial state
+  const anim = new AnimatedCanvas('c-cyclic', 3, (numX, numY) => {
+    const cyc = new Cyclic(numX, numY, () => {
+      const rand = Math.random();
+      return rand > 0.667 ? 2 : (rand > 0.333 ? 1 : 0);
+    });
+    return () => cyc.calculateNewWorld();
+  });
+  anim.drawWorld = function(world) {
+    requestAnimationFrame(() => {
+      this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+      for (let x = 0; x < this.numCellsX; x++) {
+        for (let y = 0; y < this.numCellsY; y++) {
+          if (world[y][x]) {
+            if (world[y][x] === 2) {
+              this.ctx.fillStyle = 'green';
+            } else if (world[y][x] === 1) {
+              this.ctx.fillStyle = 'blue';
+            }
+            this.ctx.fillRect(
+              x * this.gridSize,
+              y * this.gridSize,
+              this.gridSize,
+              this.gridSize);
+            }
+        }
+      }
+    });
+  };
+  anim.setBackgroundColor('#1a1423');
+  anim.setForegroundColor('#39FF14');
+  anim.updateForever();
+})();
---
+++ js/lib/cyclic.js
@@ -0,0 +1,73 @@
+// Cyclic AKA rock paper scissors - two-dimensional
+class Cyclic {
+  numCellsX = -1;
+  numCellsY = -1;
+  world = [];
+
+  constructor(numCellsX, numCellsY, cellInitFunc) {
+    this.numCellsX = numCellsX;
+    this.numCellsY = numCellsY;
+    for (let y = 0; y < numCellsY; y++) {
+      const row = [];
+      for (let x = 0; x < numCellsX; x++) {
+        if (cellInitFunc) {
+          row.push(cellInitFunc ? cellInitFunc(x, y) : 0);
+        }
+      }
+      this.world.push(row);
+    }
+  }
+
+  // naive way of calculating the next generation of the world
+  calculateNewWorld() {
+    const newWorld = [];
+    for (let y = 0; y < this.numCellsY; y++) {
+      newWorld.push(new Array(this.numCellsX).fill(0));
+    }
+    for (let y = 0; y < this.numCellsY; y++) {
+      for (let x = 0; x < this.numCellsX; x++) {
+        // count the number of neighbors with a particular value
+        const counts = { 0: 0, 1: 0, 2: 0};
+        if (y > 0) {
+          if (x > 0) {
+            counts[this.world[y - 1][x - 1]] += 1;
+          }
+          counts[this.world[y - 1][x]] += 1;
+          if (x < this.numCellsX - 1) {
+            counts[this.world[y - 1][x + 1]] += 1;
+          }
+        }
+        if (x > 0) {
+          counts[this.world[y][x - 1]] += 1;
+        }
+        if (x < this.numCellsX - 1) {
+          counts[this.world[y][x + 1]] += 1;
+        }
+        if (y < this.numCellsY - 1) {
+          if (x > 0) {
+            counts[this.world[y + 1][x - 1]] += 1;
+          }
+          counts[this.world[y + 1][x]] += 1;
+          if (x < this.numCellsX - 1) {
+            counts[this.world[y + 1][x + 1]] += 1;
+          }
+        }
+        const winnerVal = (this.world[y][x] + 1) % 3
+        // the state of the cell changes if enough neighbors have
+        // a winning value over it
+        if (counts[winnerVal] > 2) {
+          // add some randomness for smoother edges
+          if (counts[winnerVal] > 2 + ~~(Math.random() * 2)) {
+            newWorld[y][x] = winnerVal;
+          } else {
+            newWorld[y][x] = this.world[y][x];
+          }
+        } else {
+          newWorld[y][x] = this.world[y][x];
+        }
+      }
+    }
+    this.world = newWorld;
+    return newWorld;
+  }
+}
