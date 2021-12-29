50e99680168cf9be75ee38feb047bec66cebe893
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-29 17:48:10 -0500

    Consolidate rule 90 with 1d automata code

--- js/1d-automata.js
+++ js/1d-automata.js
@@ -33,6 +33,34 @@
   window.animatedCanvases.push(anim);
 }
 {
+  const RULE_NUM = 90;
+
+  const anim = new AnimatedCanvas('rule-90', 3, (numCellsX, numCellsY) => {
+    // initialize an elementary cellular automaton
+    const eca = new ECA(numCellsX, RULE_NUM, 'one_middle');
+
+    // initialize an empty world
+    const world = [];
+    for (let i = 0; i < numCellsY; i++) {
+      world.push(new Array(numCellsX).fill(0));
+    }
+
+    // the first ECA generation is the bottom row of the world
+    world.push(eca.cells);
+    world.shift();
+
+    // add new generations of the ECA to the bottom
+    return () => {
+      eca.calculateNextGeneration();
+      world.push(eca.cells);
+      world.shift();
+      return world;
+    };
+  });
+  anim.setForegroundColor('#39FF14');
+  window.animatedCanvases.push(anim);
+}
+{
   const anim = new AnimatedCanvas('1d-world-2', 3, (numCellsX, numCellsY) => {
     const eca = new ECA(numCellsX, 106, 'random');
 
