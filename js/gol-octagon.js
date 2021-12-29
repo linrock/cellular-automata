92fef3cf063fbf7df75e1b8e1e7907f3806cf6ca
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-29 16:57:25 -0500

    Put an octagon oscillator by the title, make title a live link

--- js/gol-octagon.js
+++ js/gol-octagon.js
@@ -25,8 +25,7 @@
 
     return () => gol.calculateNewWorld();
   });
-  anim.setBackgroundColor('#1a1423');
-  anim.setForegroundColor('#39FF14');
+  anim.setForegroundColor('#ffa600');
 
   setInterval(() => {
     if (anim.isAnimating) {
