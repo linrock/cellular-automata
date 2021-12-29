dcaea5b40f52137cf9d1a5f8fd0168029122db83
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-29 16:31:57 -0500

    Try putting an octagon oscillator at the top of the page

---
+++ js/gol-octagon.js
@@ -0,0 +1,36 @@
+{
+  const anim = new AnimatedCanvas('gol-octagon', 3, (numCellsX, numCellsY) => {
+    const gol = new GOL(numCellsX, numCellsY);
+
+    // manually draw an octagon oscillator
+    gol.world[0][3] = 1;
+    gol.world[1][2] = 1;
+    gol.world[2][1] = 1;
+    gol.world[3][0] = 1;
+
+    gol.world[4][0] = 1;
+    gol.world[5][1] = 1;
+    gol.world[6][2] = 1;
+    gol.world[7][3] = 1;
+
+    gol.world[0][4] = 1;
+    gol.world[1][5] = 1;
+    gol.world[2][6] = 1;
+    gol.world[3][7] = 1;
+
+    gol.world[4][7] = 1;
+    gol.world[5][6] = 1;
+    gol.world[6][5] = 1;
+    gol.world[7][4] = 1;
+
+    return () => gol.calculateNewWorld();
+  });
+  anim.setBackgroundColor('#1a1423');
+  anim.setForegroundColor('#39FF14');
+
+  setInterval(() => {
+    if (anim.isAnimating) {
+      anim.updateAndDrawWorld();
+    }
+  }, 500);
+}
