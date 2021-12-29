06ca8afae293e715c9a2f7b125d7fd2f4575f328
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-28 23:13:22 -0500

    Convert 2d trails to use animated canvas class

--- js/2d-automata-trails.js
+++ js/2d-automata-trails.js
@@ -1,107 +1,81 @@
 (() => {
-  const C_WIDTH = 330;
-  const C_HEIGHT = 165;
-  const GRID_SIZE = 3;
-
-  const UPDATE_INTERVAL_MS = 50;
-
-  const canvas = document.getElementById('game-of-life-trails');
-  canvas.width = C_WIDTH;
-  canvas.height = C_HEIGHT;
-  canvas.style.background = '#1a1423'
-
-  const ctx = canvas.getContext('2d');
-
-  let isAnimating = false;
-  canvas.addEventListener('play', () => isAnimating = true);
-  canvas.addEventListener('pause', () => isAnimating = false);
-
-  let world = [];
-
   const CELL_LIVE = 10;
 
-  // initialize the world with random numbers
-  for (let y = 0; y < C_HEIGHT / GRID_SIZE; y++) {
-    const row = [];
-    for (let x = 0; x < C_WIDTH / GRID_SIZE; x++) {
-      row.push(Math.random() > 0.8 ? CELL_LIVE : 0);
+  const anim = new AnimatedCanvas('c-game-of-life-trails', 3, (numX, numY) => {
+    // initialize the world with random numbers
+    let world = [];
+    for (let y = 0; y < numY; y++) {
+      const row = [];
+      for (let x = 0; x < numX; x++) {
+        row.push(Math.random() > 0.8 ? CELL_LIVE : 0);
+      }
+      world.push(row);
     }
-    world.push(row);
-  }
 
-  // naive way of calculating the next iteration of the world
-  function calculateNewWorld(world) {
-    const newWorld = [];
-    for (let y = 0; y < C_HEIGHT / GRID_SIZE; y++) {
-      newWorld.push([]);
-      for (let x = 0; x < C_WIDTH / GRID_SIZE; x++) {
-        // calculate the number of live neighbors
-        let numLiveNeighbors = 0;
-        if (y > 0) {
-          numLiveNeighbors +=
-            (world[y - 1][x - 1] === CELL_LIVE ? 1 : 0) +
-            (world[y - 1][x] === CELL_LIVE     ? 1 : 0) + 
-            (world[y - 1][x + 1] === CELL_LIVE ? 1 : 0);
-        }
-        numLiveNeighbors +=
-          (world[y][x - 1] === CELL_LIVE ? 1 : 0) +
-          (world[y][x + 1] === CELL_LIVE ? 1 : 0);
-        if (y < C_HEIGHT / GRID_SIZE - 1) {
-          numLiveNeighbors +=
-            (world[y + 1][x - 1] === CELL_LIVE ? 1 : 0) +
-            (world[y + 1][x] === CELL_LIVE     ? 1 : 0) +
-            (world[y + 1][x + 1] === CELL_LIVE ? 1 : 0);
-        }
-
-        // decide the new state of the current cell
-        newWorld[y][x] = world[y][x];
-        if (newWorld[y][x] === CELL_LIVE) {
-          if (numLiveNeighbors < 2 || numLiveNeighbors > 3) {
-            newWorld[y][x] = Math.max(0, newWorld[y][x] - 1);
+    // naive way of calculating the next iteration of the world
+    return () => {
+      const newWorld = [];
+      for (let y = 0; y < numY; y++) {
+        newWorld.push([]);
+        for (let x = 0; x < numX; x++) {
+          // calculate the number of live neighbors
+          let numLiveNeighbors = 0;
+          if (y > 0) {
+            numLiveNeighbors +=
+              (world[y - 1][x - 1] === CELL_LIVE ? 1 : 0) +
+              (world[y - 1][x] === CELL_LIVE     ? 1 : 0) +
+              (world[y - 1][x + 1] === CELL_LIVE ? 1 : 0);
           }
-        } else {
-          if (numLiveNeighbors === 3) {
-            newWorld[y][x] = CELL_LIVE;
+          numLiveNeighbors +=
+            (world[y][x - 1] === CELL_LIVE ? 1 : 0) +
+            (world[y][x + 1] === CELL_LIVE ? 1 : 0);
+          if (y < numY - 1) {
+            numLiveNeighbors +=
+              (world[y + 1][x - 1] === CELL_LIVE ? 1 : 0) +
+              (world[y + 1][x] === CELL_LIVE     ? 1 : 0) +
+              (world[y + 1][x + 1] === CELL_LIVE ? 1 : 0);
+          }
+
+          // decide the new state of the current cell
+          newWorld[y][x] = world[y][x];
+          if (newWorld[y][x] === CELL_LIVE) {
+            if (numLiveNeighbors < 2 || numLiveNeighbors > 3) {
+              newWorld[y][x] = Math.max(0, newWorld[y][x] - 1);
+            }
           } else {
-            newWorld[y][x] = Math.max(0, newWorld[y][x] - 1);
+            if (numLiveNeighbors === 3) {
+              newWorld[y][x] = CELL_LIVE;
+            } else {
+              newWorld[y][x] = Math.max(0, newWorld[y][x] - 1);
+            }
           }
         }
       }
-    }
-    return newWorld;
-  }
-
-  function drawWorld(world) {
-    ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
-    for (let y = 0; y < C_HEIGHT / GRID_SIZE; y++) {
-      for (let x = 0; x < C_WIDTH / GRID_SIZE; x++) {
+      world = newWorld;
+      return world;
+    };
+  });
+  anim.drawWorld = function(world) {
+    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    for (let y = 0; y < this.numCellsY; y++) {
+      for (let x = 0; x < this.numCellsX; x++) {
         if (world[y][x] !== 0) {
           if (world[y][x] === CELL_LIVE) {
-            ctx.fillStyle = '#b5ffe9';
+            this.ctx.fillStyle = '#b5ffe9';
           } else if (world[y][x] > CELL_LIVE / 2) {
-            ctx.fillStyle = '#ff66b3';
+            this.ctx.fillStyle = '#ff66b3';
           } else {
-            ctx.fillStyle = '#96031a';
+            this.ctx.fillStyle = '#96031a';
           }
-          ctx.fillRect(
-            x * GRID_SIZE,
-            y * GRID_SIZE,
-            GRID_SIZE,
-            GRID_SIZE);
+          this.ctx.fillRect(
+            x * this.gridSize,
+            y * this.gridSize,
+            this.gridSize,
+            this.gridSize);
         }
       }
     }
-  }
-
-  function updateForever() {
-    if (isAnimating) {
-      const newWorld = calculateNewWorld(world);
-      requestAnimationFrame(() => drawWorld(newWorld));
-      world = newWorld;
-    }
-    setTimeout(() => updateForever(), UPDATE_INTERVAL_MS);
-  }
-
-  drawWorld(world);
-  updateForever();
+  };
+  anim.setBackgroundColor('#1a1423');
+  anim.updateForever();
 })();
